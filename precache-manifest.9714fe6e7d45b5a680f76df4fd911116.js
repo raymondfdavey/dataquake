fe3eb6c42935d1f5536e8b264dc2f17975859e79
raymondfdavey <raymondfdavey@gmail.com>
2020-05-22 16:27:52 +0100

    Updates

---
+++ precache-manifest.9714fe6e7d45b5a680f76df4fd911116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c055470d4e157043e5e5229d2920c9f",
+    "url": "/quakedata/index.html"
+  },
+  {
+    "revision": "33f502b5f5b98f9dcd03",
+    "url": "/quakedata/static/css/main.3103aaab.chunk.css"
+  },
+  {
+    "revision": "2ba959946f30e009c62a",
+    "url": "/quakedata/static/js/2.567c1dac.chunk.js"
+  },
+  {
+    "revision": "fe1c809902957ef8baebcd7c9d3bc8f5",
+    "url": "/quakedata/static/js/2.567c1dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f502b5f5b98f9dcd03",
+    "url": "/quakedata/static/js/main.eaa590a9.chunk.js"
+  },
+  {
+    "revision": "e8e084b4dd4141f2a180",
+    "url": "/quakedata/static/js/runtime-main.0f21938e.js"
+  }
+]);
